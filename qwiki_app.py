b7823fa9b481336a4b8b0951d5bdae9593dd2490
kai-dg <haruspace.dev@gmail.com>
2020-07-03 06:55:20 -0700

    ui update

--- qwiki_app.py
+++ qwiki_app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-from ui.app import app
+from ui.app import App
 
 if __name__ == "__main__":
-    app()
+    app = App()
+    app.mainloop()
--- ui/app.py
+++ ui/app.py
@@ -6,6 +6,95 @@ def search_button():
     print("searching: ")
     return {}
 
+class App(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self._frame = None
+        self.change_frame(WikiPage)
+
+    def change_frame(self, frame):
+        new = frame(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new
+        self._frame.pack()
+
+class WikiPage(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        canvas = tk.Canvas(self, height=s.W_HEIGHT, width=s.W_WIDTH, bg=s.BACKGROUND)
+        canvas.pack(fill="both", expand=True, padx=10, pady=20)
+        # Search Bar Frame
+        frame_search = tk.Frame(canvas, bg=s.SEARCHBARBG)
+        frame_search.place(relx=0.1, rely=0, relheight=0.04, relwidth=2, anchor="n")
+        searchlabel = tk.Label(frame_search, text="Name:", bg=s.SEARCHBARBG)
+        searchlabel.config(font=(s.SEARCH_FONT, 9))
+        searchlabel.place(relx=0.45, rely=0, relwidth=0.08, relheight=1)
+        searchbar = tk.Entry(frame_search, font=(s.NORMAL_FONT, 12), bg=s.SEARCHBARFG)
+        searchbar.place(relx=0.53, rely=0, relwidth=0.34, relheight=1)
+        searchbutton = tk.Button(frame_search, text="SEARCH", font=(s.NORMAL_FONT, 9),
+                                 bg=s.BUTTON_D, fg=s.TEXT1, activebackground=s.BUTTON_A,
+                                 activeforeground=s.TEXT2,
+                                 command=lambda: search_button(searchbar.get()))
+        searchbutton.place(relx=0.87, rely=0, relwidth=0.08, relheight=1)
+        # Page Frame
+        frame_page = tk.Frame(canvas, bg=s.FOREGROUND)
+        frame_page.place(relheight=0.93, relwidth=1, relx=0.5, rely=0.04, anchor="n")
+        # Editor frame
+        frame_editor = tk.Frame(canvas, bg="red")
+        frame_editor.place(relheight=0.03, relwidth=1, relx=0.5, rely=1, anchor="s")
+        add_wiki = tk.Button(frame_editor, text="Add Page", font=(s.SEARCH_FONT, 9),
+                             bg=s.SEARCHBARBG, fg=s.TEXT2,
+                             command=lambda: parent.change_frame(AddPage))
+        add_wiki.place(relwidth=0.25, relheight=1)
+        update_wiki = tk.Button(frame_editor, text="Update Page", font=(s.SEARCH_FONT, 9),
+                                bg=s.SEARCHBARBG, fg=s.TEXT2)
+        update_wiki.place(relx=0.25, relwidth=0.25, relheight=1)
+        delete_wiki = tk.Button(frame_editor, text="Delete Page", font=(s.SEARCH_FONT, 9),
+                                bg=s.SEARCHBARBG, fg=s.TEXT2)
+        delete_wiki.place(relx=0.5, relwidth=0.25, relheight=1)
+        change_wiki = tk.Button(frame_editor, text="Change Wiki", font=(s.SEARCH_FONT, 9),
+                                bg=s.SEARCHBARBG, fg=s.TEXT2)
+        change_wiki.place(relx=0.75, relwidth=0.25, relheight=1)
+
+class AddPage(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        canvas = tk.Canvas(self, height=s.W_HEIGHT, width=s.W_WIDTH, bg=s.BACKGROUND)
+        canvas.pack(fill="both", expand=True, padx=10, pady=20)
+        # Search Bar Frame
+        frame_search = tk.Frame(canvas, bg=s.SEARCHBARBG)
+        frame_search.place(relx=0.1, rely=0, relheight=0.04, relwidth=2, anchor="n")
+        searchlabel = tk.Label(frame_search, text="Name:", bg=s.SEARCHBARBG)
+        searchlabel.config(font=(s.SEARCH_FONT, 9))
+        searchlabel.place(relx=0.45, rely=0, relwidth=0.08, relheight=1)
+        searchbar = tk.Entry(frame_search, font=(s.NORMAL_FONT, 12), bg=s.SEARCHBARFG)
+        searchbar.place(relx=0.53, rely=0, relwidth=0.34, relheight=1)
+        searchbutton = tk.Button(frame_search, text="SEARCH", font=(s.NORMAL_FONT, 9),
+                                 bg=s.BUTTON_D, fg=s.TEXT1, activebackground=s.BUTTON_A,
+                                 activeforeground=s.TEXT2,
+                                 command=lambda: [parent.change_frame(WikiPage), search_button()])
+        searchbutton.place(relx=0.87, rely=0, relwidth=0.08, relheight=1)
+        # Page Frame
+        frame_page = tk.Frame(canvas, bg="red")
+        frame_page.place(relheight=0.93, relwidth=1, relx=0.5, rely=0.04, anchor="n")
+        # Editor frame
+        frame_editor = tk.Frame(canvas, bg="red")
+        frame_editor.place(relheight=0.03, relwidth=1, relx=0.5, rely=1, anchor="s")
+        add_wiki = tk.Button(frame_editor, text="Add Page", font=(s.SEARCH_FONT, 9),
+                             bg=s.SEARCHBARBG, fg=s.TEXT2,
+                             command=lambda: parent.change_frame(AddPage))
+        add_wiki.place(relwidth=0.25, relheight=1)
+        update_wiki = tk.Button(frame_editor, text="Update Page", font=(s.SEARCH_FONT, 9),
+                                bg=s.SEARCHBARBG, fg=s.TEXT2)
+        update_wiki.place(relx=0.25, relwidth=0.25, relheight=1)
+        delete_wiki = tk.Button(frame_editor, text="Delete Page", font=(s.SEARCH_FONT, 9),
+                                bg=s.SEARCHBARBG, fg=s.TEXT2)
+        delete_wiki.place(relx=0.5, relwidth=0.25, relheight=1)
+        change_wiki = tk.Button(frame_editor, text="Change Wiki", font=(s.SEARCH_FONT, 9),
+                                bg=s.SEARCHBARBG, fg=s.TEXT2)
+        change_wiki.place(relx=0.75, relwidth=0.25, relheight=1)
+
 def app():
     root = tk.Tk()
     root.title(s.TITLE)
